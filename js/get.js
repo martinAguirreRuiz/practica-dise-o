001bfce090be993368cd1f202d3d4a6fa7006f82
martinAguirreRuiz <martinfaguirreruiz@gmail.com>
2024-05-21 20:19:03 -0300

    cambio url del get

--- js/get.js
+++ js/get.js
@@ -1,7 +1,7 @@
 export async function getProducts(){
     try {
         
-        const response = await fetch("http://localhost:3000/productos");
+        const response = await fetch("https://practica-disenio.vercel.app/productos");
         const productos = await response.json();
 
         insertProducts(productos);
